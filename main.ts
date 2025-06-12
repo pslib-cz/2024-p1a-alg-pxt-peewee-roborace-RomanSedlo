cdbb309d9e40869fcfafdd5a8bfbf99cf4624c6d
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-06-12 18:21:32 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,50 @@
-type IRC = {
-    l: DigitalPin,
-    c: DigitalPin,
-    r: DigitalPin
-}
-type PinData = {
-    l: boolean,
-    c: boolean,
-    r: boolean
-}
-const IR: IRC = {
-    l: DigitalPin.P14,
+type drivingSignal = {
+    x: number;
+    y: number;
+    z: number
+};
+type lightDirection = {
+    c: DigitalPin;
+    r: DigitalPin;
+    l: DigitalPin
+};
+type data = {
+    c: number;
+    r: number;
+    l: number
+};
+
+const IR: lightDirection = {
     c: DigitalPin.P15,
-    r: DigitalPin.P13
+    r: DigitalPin.P13,
+    l: DigitalPin.P14
+};
+
+let expectedSender = -1584843917;
+let ready: boolean;
+let drivingPackage: drivingSignal;
+let dataPack: data = { c: 0, r: 0, l: 0 };
+let speed: number = 220;
+
+function driveGo(dataPack: data) {
+    if (dataPack.c === 1) {
+
+    } else {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
+        PCAmotor.MotorRun(PCAmotor.Motors.M2, speed)
+    }
 }
-pins.setPull(IR.l, PinPullMode.PullNone);
-pins.setPull(IR.c, PinPullMode.PullNone);
-pins.setPull(IR.r, PinPullMode.PullNone);
 
-let data: PinData = {l: false, c: false, r: false};
+pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
+pins.digitalReadPin(DigitalPin.P8)
+pins.setPull(IR.c, PinPullMode.PullNone)
+pins.setPull(IR.r, PinPullMode.PullNone)
+pins.setPull(IR.l, PinPullMode.PullNone)
+
 basic.forever(function () {
-    data.l = pins.digitalReadPin(IR.l) === 1;
-    data.c = pins.digitalReadPin(IR.c) === 1;
-    data.r = pins.digitalReadPin(IR.r) === 1;
-    console.log(data);
-
+    dataPack.c = pins.digitalReadPin(IR.c)
+    dataPack.r = pins.digitalReadPin(IR.r)
+    dataPack.l = pins.digitalReadPin(IR.l)
+    driveGo(dataPack)
     basic.pause(20)
 })
