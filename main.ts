7538a0744570b644be25af2d3efdd6d386492702
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-06-16 09:13:00 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -24,7 +24,7 @@ pins.setPull(IR.r, PinPullMode.PullNone);
 pins.setPull(IR.l, PinPullMode.PullNone);
 
 let dataPack: data = { c: 0, r: 0, l: 0 }
-let speed: number = 130;
+let speed: number = 180;
 let vojta: number = 2; //speed divider
 let knedlik: number = 0.8;
 let posledniZatacka: string;
